1d1b96a4eb211a5ee6518793fd5400d875ccc787
Brandon-Magofna <magofna68@gmail.com>
2021-04-14 13:26:59 -0700

    add properties to our Player constructor
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

--- __tests__/RPG.test.js
+++ __tests__/RPG.test.js
@@ -1,16 +1,47 @@
 import { Person } from '../src/classes.js'
 
 describe('PersonClass', () => {
+  let myPerson;
+  beforeEach(() => {
+    myPerson = new Person()
+  })
 
   test('should correctly create a Person Class', () => {
-    const examplePerson = new Person()
-    expect(examplePerson).toBeDefined();
+    expect(myPerson).toBeDefined();
   })
 
   test('Person class should have health property', () => {
-    const myHealth = new Person() 
-    expect(myHealth.health).toBeDefined()
+    expect(myPerson.health).toBeDefined()
   })
+
+  test('Person class should have health default value of 100', () => {
+    expect(myPerson.health).toEqual(100)
+  })
+
+  test('Person class should have attack default value of 100', () => {
+    expect(myPerson.attack).toEqual(100)
+  })
+
+  test('Person class should have speed default value of 100', () => {
+    expect(myPerson.speed).toEqual(100)
+  })
+
+  test('Person class should have defense default value of 100', () => {
+    expect(myPerson.defense).toEqual(100)
+  })
+
+  test('Person class should have stamina default value of 100', () => {
+    expect(myPerson.stamina).toEqual(100)
+  })
+
+  test('Person class should contain an inventory object', () => {
+    expect(myPerson.inventory).toBeDefined()
+  })
+
+  test('Inventory object contains armor', () => {
+    expect(myPerson.inventory.armor).toBeDefined()
+  })
+
 })
 
 
--- src/classes.js
+++ src/classes.js
@@ -1,8 +1,13 @@
 export class Person {
   constructor() {
-    this.health = 0;
+    this.health = 100;
+    this.attack = 100;
+    this.speed = 100;
+    this.defense = 100;
+    this.stamina = 100;
+    this.inventory = {
+      armor: []
+    };
   }
 }
-
-
 
